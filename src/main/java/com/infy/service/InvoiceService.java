6bc02c91171f9151fc4f7e2bc15a306121e18e53
SujayAgrawal <30735216+SujayAgrawal@users.noreply.github.com>
2020-04-13 11:41:34 +0530

    integrated with spring sleuth

--- src/main/java/com/infy/service/InvoiceService.java
+++ src/main/java/com/infy/service/InvoiceService.java
@@ -1,6 +1,7 @@
 package com.infy.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,11 +15,13 @@ public class InvoiceService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Value("${sales.order.uri}")
+	private String salesOrderUri;
+
 	// use of rest template to call sales order svc
 	// no need of dao layer here as we will not be connecting to database
 	public Invoice createInvoice(Customer customer) {
-		SalesOrder salesOrder = restTemplate.getForObject("http://localhost:8081/orders/{orderId}", SalesOrder.class,
-				customer.getOrderId());
+		SalesOrder salesOrder = restTemplate.getForObject(salesOrderUri, SalesOrder.class, customer.getOrderId());
 		String amount = salesOrder.getSalesOrderAmount();
 		Invoice invoice = new Invoice();
 		invoice.setCustomer(customer);
